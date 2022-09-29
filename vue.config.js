8c32d1c4ff787f764575aa1ce95d8faf40599420
hexingmeng <hxm0210@qq.com>
2022-09-29 10:07:46 +0800

    feat(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,27 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "./",
   devServer: {
+    // 端口号
     port: 8888,
+    // 主机名
     host: "localhost",
+    // 启动项目时是否自动打开浏览器
     open: true,
+    // 是否开启https
     https: false,
-  }
+    // 配置跨域
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+
+    }
+  },
+  lintOnSave: false
 })
